e6230b2b1692ccea9711bea8db7483b2ef1f29ed
MethmaW <methma.cw@gmail.com>
2021-06-14 04:39:07 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { lorem } from "faker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'typing-challenge';
+	randomText: string = lorem.sentence();
+	userText: string = "";
+	correctText: boolean = false;
+
+	onChangeText(value: string) {
+		this.userText = value;
+	}
+
+	changeColor(randomLetter: string, enteredLetter: string) {
+		if (!enteredLetter) {
+			return "pending";
+		}
+
+		return randomLetter === enteredLetter ? "correct" : "incorrect";
+	}
 }
